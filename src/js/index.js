b89c555fb9ab3013ae345c1cc76e873a43539b30
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-03-12 22:30:29 -0300

    Site the last of us completo

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagesCarrossel = document.querySelectorAll('.imagem');
+let carrosselActive = 0;
+let timeout;
+
+function desactiveCarrosselActive() {
+  botoesCarrossel.forEach((button) => button.classList.remove('selecionado'));
+  imagesCarrossel.forEach((button) => button.classList.remove('ativa'));
+}
+
+function activeCarrossel() {
+  botoesCarrossel[carrosselActive].classList.add('selecionado');
+  imagesCarrossel[carrosselActive].classList.add('ativa');
+}
+
+function changeCarrosselActive(index) {
+  carrosselActive = index;
+  desactiveCarrosselActive();
+  activeCarrossel();
+  autoCarrossel();
+}
+botoesCarrossel.forEach((button, i) =>
+  button.addEventListener('click', () => changeCarrosselActive(i)),
+);
+
+function autoCarrossel() {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    if (carrosselActive < botoesCarrossel.length - 1) {
+      changeCarrosselActive(carrosselActive + 1);
+    } else if (carrosselActive === botoesCarrossel.length - 1) {
+      changeCarrosselActive(0);
+    }
+  }, 2000);
+}
+autoCarrossel();
